d81e9019dedd6ec0b196cce02da0194e744c0834
Saransh Gupta <saransh2907@outlook.com>
2025-10-27 21:01:27 +0530

    initial commit

---
+++ src/test/java/com/trackfin/fintrack/FintrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackfin.fintrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
